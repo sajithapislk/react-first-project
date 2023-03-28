536039c6e73d651ec513018e30488db090b6bd02
sajithapislk <sajith@apis.lk>
2023-03-28 17:42:24 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Products from './Pages/Products';
+import NavBar from './Components/NavBar';
 
 const App = () => {
- return (
+  return (
     <>
-       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/about" element={<About />} />
-       </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
- );
+  );
 };
 
 export default App;
